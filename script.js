d86c77ebe89aa510593e345a84c4452ab00d44ed
satya prakash swain <satyasitu14@gmail.com>
2024-03-15 23:15:12 +0530

    added

---
+++ script.js
@@ -0,0 +1,14 @@
+<<<<<<< HEAD
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+=======
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+>>>>>>> a42dfe262fcd8aed8250d7e09ef52e2c82767444
+}
